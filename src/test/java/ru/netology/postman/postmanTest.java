a69c2cc8941ebf043897efcb15b547e2bdf28430
Элина Хафизова <elina6hafizova@yandex.ru>
2022-08-07 17:18:18 +0300

    failed test

--- src/test/java/ru/netology/postman/postmanTest.java
+++ src/test/java/ru/netology/postman/postmanTest.java
@@ -21,7 +21,7 @@ public class postmanTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("some data", equalTo("some data"))
         ;
 
     }
