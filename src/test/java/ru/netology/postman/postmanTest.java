4f623c8e88653f6b1abcdc1ab3ee7078b770fd0c
Элина Хафизова <elina6hafizova@yandex.ru>
2022-08-07 16:28:02 +0300

    initial commit

---
+++ src/test/java/ru/netology/postman/postmanTest.java
@@ -0,0 +1,29 @@
+package ru.netology.postman;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postmanTest {
+
+
+    @Test
+    void shouldPostBody() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+
+    }
+
+}
